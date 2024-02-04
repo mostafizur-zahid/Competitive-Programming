c30c996aab621ec4694fd8ca36160d2c8d1efab5
MD Mostafizur Rahman Zahid <mostafizurrahmanzahid8@gmail.com>
2024-02-04 12:37:29 +0600

    update

---
+++ 01. Vector/vector_input.cpp
@@ -0,0 +1,22 @@
+
+#include<bits/stdc++.h>
+using namespace std;
+
+int main(){
+    int n;
+    vector<int>v;
+
+    cin>>n;
+    for(int i=0;i<n;i++){
+        int a;
+        cin>>a;
+        v.push_back(a);
+    }
+    cout<<"Vector size is: "<<v.size()<<endl;
+    cout<<"these are:"<<endl;
+
+    for(int i=0;i<v.size();i++)
+    cout<<v[i]<<" ";
+
+    cout<<endl;
+}
